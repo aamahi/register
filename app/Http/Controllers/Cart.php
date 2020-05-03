e8a0159f40d8d2d5df4ea7f0b9eccb1f4fcab217
Mahi <RapaMahi>
2020-05-03 16:09:18 +0600

    Cannot add over product in product _quantity

--- app/Http/Controllers/Cart.php
+++ app/Http/Controllers/Cart.php
@@ -7,21 +7,29 @@ use Illuminate\Http\Request;
 class Cart extends Controller
 {
     public function add_cart(Request $request){
-        $ip_address = request()->ip();
-        $cart_add = [];
-        $cart_add['product_id'] = $request->product_id;
-        $cart_add['ip_address'] = $ip_address;
-        $cart_add['quantity'] = $request->quantity;
-        if (\App\Model\Cart::where('product_id',$request->product_id)->where('ip_address',$ip_address)->exists()){
-            \App\Model\Cart::where('product_id',$request->product_id)->where('ip_address',$ip_address)->increment('quantity',$request->quantity);
-        }else{
-            $cart_add = \App\Model\Cart::insert($cart_add);
+        if($request->quantity>\App\Model\Product::find($request->product_id)->quantity){
+            $notification = array(
+                'message' => "Product is too sort",
+                'alert-type' => 'info'
+            );
+            return redirect()->back()->with($notification);
+        }else {
+            $ip_address = request()->ip();
+            $cart_add = [];
+            $cart_add['product_id'] = $request->product_id;
+            $cart_add['ip_address'] = $ip_address;
+            $cart_add['quantity'] = $request->quantity;
+            if (\App\Model\Cart::where('product_id', $request->product_id)->where('ip_address', $ip_address)->exists()) {
+                \App\Model\Cart::where('product_id', $request->product_id)->where('ip_address', $ip_address)->increment('quantity', $request->quantity);
+            } else {
+                $cart_add = \App\Model\Cart::insert($cart_add);
+            }
+            $notification = array(
+                'message' => "Product Add to Cart",
+                'alert-type' => 'success'
+            );
+            return redirect()->back()->with($notification);
         }
-        $notification = array(
-            'message' => "Product Add to Cart",
-            'alert-type' => 'success'
-        );
-        return redirect()->back()->with($notification);
     }
    public function cart_remove($id){
         $delete_product_form_cart = \App\Model\Cart::find($id)->delete();
